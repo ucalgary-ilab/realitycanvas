c28333b8a7e4ad988acf5422f190d4db4a1778f8
Zhijie Xia <zhijiexiacs@gmail.com>
2022-06-13 09:47:09 -0600

    Proof of Concept: matter.js can be stopped and rerun

--- reality-canvas/src/physic.js
+++ reality-canvas/src/physic.js
@@ -69,9 +69,16 @@ var boxB = Bodies.rectangle(450, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 // add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground]);
-// run the renderer
-// Render.run(render);
+Render.run(render);
 // create runner
 var runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
+setTimeout(() => {
+    Render.stop(render);
+    Runner.stop(runner);
+}, 3000);
+setTimeout(() => {
+    Render.run(render);
+    Runner.run(runner, engine);
+}, 10000);
