b8d11ddbcb1bd8e450a5a0cbbdb48be59f4926ca
Zhijie Xia <zhijiexiacs@gmail.com>
2022-08-16 19:34:22 -0600

    Stomp Action

--- reality-canvas/src/action.js
+++ reality-canvas/src/action.js
@@ -16,7 +16,7 @@ export default class Action {
 
 
     detectAction(bodyParts) {
-        if (this.type == "Wave") {
+        if (this.type == "Wave") { // left 
             if (this.triggered) {
                 return;
             }
@@ -46,6 +46,49 @@ export default class Action {
                 this.actionInitial = true;
             }
             else if (this.actionInitial && angle >= 90) {
+                // console.log("completed", angle);
+                this.actionComplete = true;
+            }
+
+            if (this.actionComplete) {
+                console.log("Triggered");
+                this.triggered = true;
+
+                this.actionInitial = false;
+                this.actionComplete = false;
+            }
+        }
+
+        if (this.type == "Stomp") { // left
+            if (this.triggered) {
+                return;
+            }
+
+            let A = { x: bodyParts[23].x * this.WIDTH, y: bodyParts[23].y * this.HEIGHT };
+            let B = { x: bodyParts[25].x * this.WIDTH, y: bodyParts[25].y * this.HEIGHT };
+            let C = { x: bodyParts[27].x * this.WIDTH, y: bodyParts[27].y * this.HEIGHT };
+            let AB = Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
+            let BC = Math.sqrt(Math.pow(B.x - C.x, 2) + Math.pow(B.y - C.y, 2));
+            let AC = Math.sqrt(Math.pow(C.x - A.x, 2) + Math.pow(C.y - A.y, 2));
+            let angle = Math.acos((BC * BC + AB * AB - AC * AC) / (2 * BC * AB)) * 180 / Math.PI;
+
+            if (A.x < 0 || A.y < 0) {
+                return;
+            }
+            if (B.x < 0 || B.y < 0) {
+                return;
+            }
+            if (C.x < 0 || B.y < 0) {
+                return;
+            }
+            // console.log(angle);
+
+            // console.log(A, B, C, angle);
+            if (angle < 120) {
+                // console.log("initialized", angle);
+                this.actionInitial = true;
+            }
+            else if (this.actionInitial && angle >= 150) {
                 // console.log("completed", angle);
                 this.actionComplete = true;
             }
